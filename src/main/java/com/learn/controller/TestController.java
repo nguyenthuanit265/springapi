2e57e9d7008bd3b75408db993415f7342dff603b
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-06-25 09:55:56 +0700

    update test, build ok

--- src/main/java/com/learn/controller/TestController.java
+++ src/main/java/com/learn/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.learn.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/secure")
-    public String secureEndpoint() {
-        return "This is a secure endpoint";
+    public ResponseEntity<?> secureEndpoint() {
+        return ResponseEntity.ok("This is a secure endpoint");
     }
 
     @GetMapping("/public")
-    public String publicEndpoint() {
-        return "This is a public endpoint v1";
+    public ResponseEntity<?> publicEndpoint() {
+        return ResponseEntity.ok("This is a public endpoint v1");
     }
 }
